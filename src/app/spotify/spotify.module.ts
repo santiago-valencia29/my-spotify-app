4dc8e026f7c430c427af254d505a6db4a2934a5a
Santiago <edy-santiago@outlook.com>
2021-07-16 19:29:03 -0500

    install angular material

--- src/app/spotify/spotify.module.ts
+++ src/app/spotify/spotify.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpotifyComponent } from './spotify.component';
+import { AngularMaterialModule } from '../app-material.module';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [SpotifyComponent]
+  imports: [CommonModule, AngularMaterialModule],
+  exports: [],
+  declarations: [SpotifyComponent],
 })
-export class SpotifyModule { }
+export class SpotifyModule {}
