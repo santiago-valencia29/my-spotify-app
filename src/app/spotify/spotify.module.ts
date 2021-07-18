064358b83995273bc8adb62862a9e77c06034795
Santiago <edy-santiago@outlook.com>
2021-07-18 15:22:23 -0500

    create pipe noimage api

--- src/app/spotify/spotify.module.ts
+++ src/app/spotify/spotify.module.ts
@@ -4,10 +4,11 @@ import { AngularMaterialModule } from '../app-material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
+import { NoimagePipe } from './pipes/noimage.pipe';
 
 @NgModule({
   imports: [CommonModule, AngularMaterialModule, FlexLayoutModule],
   exports: [HomeComponent, SearchComponent],
-  declarations: [HomeComponent, SearchComponent],
+  declarations: [HomeComponent, SearchComponent, NoimagePipe],
 })
 export class SpotifyModule {}
